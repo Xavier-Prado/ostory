1f2cfb79cdb9f6142376770b9eb790f14f34a506
ThomasChantreuil <chantreuil.thomas@gmail.com>
2022-10-05 17:01:03 +0200

    mv logincontroller to api folder

--- src/Controller/Api/LoginController.php
+++ src/Controller/Api/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Api;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
@@ -16,7 +16,9 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 
-
+/**
+ * @Route("/api")
+ */
 class LoginController extends AbstractController
 {
     /**
