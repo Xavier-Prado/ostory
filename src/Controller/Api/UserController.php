89f3ebfc49f34d5353bdc585948586d61580b9eb
CoralieFleury <fleurycoralie1@gmail.com>
2022-10-06 14:35:21 +0200

    acl api

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -2,14 +2,12 @@
 
 namespace App\Controller\Api;
 
-use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
